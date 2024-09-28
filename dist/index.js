c9d1c8378014a48bb46ff3cc71411907d9a5f4cc
Anubhav <anubhav_a@me.iitr.ac.in>
2024-09-28 13:25:48 +0530

    added my intro on get request

--- dist/index.js
+++ dist/index.js
@@ -7,10 +7,15 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const identityController_1 = __importDefault(require("./constollers/identityController"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
 app.use(body_parser_1.default.json());
 app.use((0, cors_1.default)());
+app.get("/", (req, res) => {
+    const filePath = path_1.default.join(__dirname, '../index.html');
+    res.sendFile(filePath);
+});
 app.post("/identify", (req, res) => {
     console.log(req.body);
     (0, identityController_1.default)(req, res);
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,18 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import { Request, Response } from "express";
 import indentify from "./constollers/identityController";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
+
+app.get("/", (req: Request, res: Response)=>{
+  const filePath = path.join(__dirname, '../index.html');
+  res.sendFile(filePath);
+})
+
 
 app.post("/identify", (req: Request, res: Response) => {
     console.log(req.body);
