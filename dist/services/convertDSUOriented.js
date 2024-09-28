0fcc749fdc9fe2dda8b279293c3210e4057e5d11
Anubhav <anubhav_a@me.iitr.ac.in>
2024-09-28 13:35:07 +0530

    removed unnecessary files

--- dist/services/convertDSUOriented.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const convertDSUOriented = (contacts) => {
-    return contacts.map((c) => ({ id: c.id, linkedId: c.linkedId || null }));
-};
-exports.default = convertDSUOriented;
--- src/services/convertDSUOriented.ts
+++
@@ -1,7 +0,0 @@
-import { contact } from "../types";
-
-const convertDSUOriented = (contacts : contact[]) =>{
-    return contacts.map((c) => ({id:c.id, linkedId: c.linkedId || null}));
-  }
-
-  export default convertDSUOriented;
